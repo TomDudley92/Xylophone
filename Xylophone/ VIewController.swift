7b875565dfeb35952f850215d2b3c86817a47856
Tom Dudley <thomas.dudley@digital.hmrc.gov.uk>
2018-09-09 19:53:00 +0100

    Added xylophone app

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,39 @@
 //
 
 import UIKit
+import AudioToolbox
+//import AVFoundation
 
 class ViewController: UIViewController{
-    
 
+//    var audioPlayer: AVAudioPlayer?
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
         
+//        let xylophoneSound = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")!
+//
+//        do {
+//            audioPlayer = try AVAudioPlayer(contentsOf: xylophoneSound)
+//            guard let audioPlayer = audioPlayer else { return }
+//
+//            audioPlayer.prepareToPlay()
+//            audioPlayer.play()
+//        } catch let error as Error {
+//            print(error)
+//        }
+        
+        if let soundUrl = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav"){
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundUrl as CFURL, &mySound)
+            //Play
+            AudioServicesPlaySystemSound(mySound)
+        }
         
         
     }
-    
-  
-
 }
-
